270d102de1b76ba01ef36cede8cf64ed2681381d
Kim Vinberg <support@websitecare.io>
2020-11-28 16:23:24 +0100

    index.php ready for v1.0.0

--- index.php
+++ index.php
@@ -1,4 +1,11 @@
 <?php
+/*
+* wcioShop
+* Version 1.0.0
+* Author: Kim Vinberg <support@websitecare.io>
+* Source: https://github.com/websitecareio/wcioShop
+* License: https://github.com/websitecareio/wcioShop/blob/master/LICENSE
+ */
 session_start();
 
 require(dirname(__FILE__) . '/inc/db.php'); //connect to database
@@ -17,58 +24,28 @@ $smarty->template_dir   = $templateDir; //Template dir
 $smarty->assign('template_dir', $smartyTemplateDir);
 
 // Load all shop settings from databse
-$stmt = $dbh->prepare("SELECT type,value1 FROM wcio_se_settings");
+$stmt = $dbh->prepare("SELECT columnName,columnValue FROM wcio_se_settings");
 $result = $stmt->execute();
-while($setting = $stmt->fetch( PDO::FETCH_ASSOC )) {
+while($setting = $stmt->fetch( PDO::FETCH_ASSOC ))
 
-      $_SETTING[$setting['type']] = $setting['value1'];
-      $smarty->assign('setting'.ucfirst($setting['type']).'',$setting['value1']); // Save setting for smarty
+      // Assign values to be used in files
+      $_SETTING[$setting['columnName']] = $setting['columnValue'];
+
+      // Assign values to smarty for use in templates.
+      $smarty->assign('setting'.ucfirst($setting['columnName']).'',$setting['columnValue']); // Save setting for smarty
 
 }
 
-// SEO : Load the current URL from permalinks table and load meta
+// SEO : Load the current URL from permalinks including meta for this URL
+include(dirname(__FILE__) . '/inc/seo.php');
 
-
-// Ready to load content
-
-/* INDEX */
-$smartyTemplateFile = "index.tpl";
 // If no cache of this page is done, then we need to load all functions etc to make the cache file.
-
 if (!$smarty->isCached($smartyTemplateFile, $smartyTemplateFile)) {
 
-    // Load only the template-fucntions that is used in the template files.
-    $templateFiles = array(
-        "$smartyTemplateFile", // This is the current template file
-        "template-parts/head.tpl", // This is required template file
-        "template-parts/header.tpl", // This is required template file
-        "template-parts/footer.tpl" // This is required template file
-    );
+      // Load template functions
+      include(dirname(__FILE__) . '/inc/templateFunctions.php');
 
-    foreach ($templateFiles as $key => $templateFile) {
-
-        if(file_exists($templateDir . $templateFile)) {
-              $fc = file_get_contents($templateDir . $templateFile);
-
-              $data = array();
-              preg_match_all('/\$([a-zA-Z0-9]*)/is', $fc, $data, PREG_PATTERN_ORDER);
-              unset($data[0]);
-              $data = $data[1];
-              $data = array_unique($data); // Array is now (1, 2, 3)
-
-              foreach ($data as $templateFunction) {
-
-                  $templateFunctionFile = dirname(__FILE__) . "/inc/template-functions/$templateFunction.php";
-
-                  if (file_exists($templateFunctionFile)) {
-
-                      // Inclkue the php file to be used in template..
-                      include($templateFunctionFile);
-                  }
-              }
-        }
-    }
 } //end if cache
 
-
+// Display the page and all its functions
 $smarty->display($smartyTemplateFile, $smartyTemplateFile);
