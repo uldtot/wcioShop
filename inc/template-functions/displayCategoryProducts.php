145e5d3b00609d96c3920b8f6afdf586b476e9d4
Kim Vinberg <support@websitecare.io>
2020-11-29 20:19:48 +0100

    Removed unnessary database call .

--- inc/template-functions/displayCategoryProducts.php
+++ inc/template-functions/displayCategoryProducts.php
@@ -4,15 +4,6 @@
 
     $displayCategoryProducts = array();
 
-    $stmt = $dbh->prepare("SELECT * FROM wcio_se_categories WHERE id=:get_id");
-    $result = $stmt->execute(array(
-        ":get_id" => $get_id
-    ));
-    $SeCategoryCount = $stmt->rowCount();
-
-    if ($SeCategoryCount > '0')
-    {
-        $data_category = $stmt->fetch(PDO::FETCH_ASSOC);
 
         $stmt = $dbh->prepare("SELECT prdid FROM wcio_se_product_categories WHERE catid=:get_id");
         $result = $stmt->execute(array(
@@ -23,7 +14,7 @@
         while ($data_category_products = $stmt->fetch(PDO::FETCH_ASSOC))
         {
 
-            $query_products = $dbh->prepare("SELECT * FROM wcio_se_products WHERE id=:prdid");
+            $query_products = $dbh->prepare("SELECT id,name,price,discount,shorttext,stock FROM wcio_se_products WHERE id=:prdid");
             $result = $query_products->execute(array(
                 ":prdid" => $data_category_products['prdid']
             ));
@@ -35,7 +26,7 @@
                     'prdid' => $data_products['id'],
                     'name' => $data_products['name'],
                     'price' => $data_products['price'],
-                    'file2' => $data_products['file2'],
+                    'image' => $data_products['file2'],
                     'discount' => $data_products['discount'],
                     'shorttext' => $data_products['shorttext'],
                     'stock' => $data_products['stock'],
@@ -50,6 +41,5 @@
         }
         //while
 
-    }
 
     $smarty->assign("displayCategoryProducts", $displayCategoryProducts);
