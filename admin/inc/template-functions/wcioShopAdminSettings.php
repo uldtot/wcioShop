625d5e4a858ff3fd6091b511b11c3d4b3d99a1d6
Kim Vinberg aKa Uldtot <kim@dicm.dk>
2025-11-22 02:06:33 +0100

    Update wcioShopAdminSettings.php
    Updated to work with custom settings

--- admin/inc/template-functions/wcioShopAdminSettings.php
+++ admin/inc/template-functions/wcioShopAdminSettings.php
@@ -2,14 +2,47 @@
 
 $wcioShopAdminSettings = array();
 
-$stmt = $dbh->prepare("SELECT * FROM wcio_se_settings WHERE settingMainGroup = 'Store settings' ORDER BY settingSecondaryGroup,settingOrder,columnNiceName");
+// Check if we need specific settings of just default
+$mainGroup = $_GET["setting"] ?? "";
+
+if($mainGroup) {
+    
+    $stmt = $dbh->prepare("SELECT * FROM wcio_se_settings WHERE columnValue = :mainGroup AND columnName = 'wcioShopAdminSettingsMenu' LIMIT 1");
+    $stmt->bindParam(':mainGroup', $mainGroup, PDO::PARAM_STR);
+    $result = $stmt->execute();
+    
+    $dataMain = $stmt->fetch(PDO::FETCH_ASSOC);
+    
+    /*
+    Array
+    (
+        [id] => 48
+        [autoload] => 0
+        [settingOrder] => 0
+        [columnName] => wcioShopAdminSettingsMenu
+        [columnNiceName] => Mintenance Mode
+        [settingMainGroup] => Maintenance mode
+        [settingSecondaryGroup] => Maintenance mode
+        [columnType] => 
+        [columnTypeData] => 
+        [columnValue] => maintenancemode
+        [columnDescription] => 
+    )*/
+
+}
+
+// Now fetch data
+$settingMainGroup = $dataMain["settingMainGroup"] ?? "Store settings";
+$stmt = $dbh->prepare("SELECT * FROM wcio_se_settings WHERE settingMainGroup = :settingMainGroup AND columnName != 'wcioShopAdminSettingsMenu' ORDER BY settingSecondaryGroup,settingOrder,columnNiceName");
+$stmt->bindParam(':settingMainGroup', $settingMainGroup, PDO::PARAM_STR);
 $result = $stmt->execute();
+
 
   while($data = $stmt->fetch(PDO::FETCH_ASSOC))
   {
 
         $settingMainGroup = $data['settingMainGroup'];
-        $settingSecondaryGroup = $data['settingSecondaryGroup'];
+        $settingSecondaryGroup = $dataMain['settingSecondaryGroup'];
 
         $wcioShopAdminSettings[$settingMainGroup][$settingSecondaryGroup][] = array
         (
